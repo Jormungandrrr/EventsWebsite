99aad7006aefa132028f518f64860c0b225606d3
Jormungandrrr <j.verstijlen@gmail.com>
2016-06-14 14:25:44 +0200

    UserDB aangemaakt

--- EventsWebsite/EventsWebsite/Database/UserDB.cs
+++ EventsWebsite/EventsWebsite/Database/UserDB.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using EventsWebsite.Models;
 
 namespace EventsWebsite.Database
 {
     public class UserDB : Database
     {
-        private void InsertPerson()
+        private void InsertPerson(UserModel User)
         {
-            Dictionary<string,>
+            Dictionary<string, string> PersonData = new Dictionary<string, string>();
+            PersonData.Add("voornaam",User.Name);
+            Insert("Persoon",PersonData);
+
+            Dictionary<string, string> AccountData = new Dictionary<string, string>();
+            PersonData.Add("gebruikersnaam", User.Username);
+            PersonData.Add("email", User.Email);
+            PersonData.Add("geactiveerd", "1");
+
         }
     }
 }
