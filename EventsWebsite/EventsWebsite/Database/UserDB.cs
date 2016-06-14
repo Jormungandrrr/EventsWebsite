186a90334e9aa21742b0f66d6b36ab057ba2662b
Jormungandrrr <j.verstijlen@gmail.com>
2016-06-14 14:19:09 +0200

    Database count

---
+++ EventsWebsite/EventsWebsite/Database/UserDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventsWebsite.Database
+{
+    public class UserDB : Database
+    {
+        private void InsertPerson()
+        {
+            Dictionary<string,>
+        }
+    }
+}
