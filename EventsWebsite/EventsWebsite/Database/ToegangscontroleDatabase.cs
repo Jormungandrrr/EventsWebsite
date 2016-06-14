0b228cc7f217f3e69f977e036f01ec2f837eb9ab
FireRageNL <r.vanoldenbeek@student.fontys.nl>
2016-06-14 13:08:09 +0200

    testproject

---
+++ EventsWebsite/EventsWebsite/Database/ToegangscontroleDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsWebsite.Database
+{
+    class ToegangscontroleDatabase : Database
+    {
+        public bool HasAccess(int barcode)
+        {
+            return false;
+        }
+    }
+}
