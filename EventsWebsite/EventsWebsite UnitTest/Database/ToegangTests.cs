76c53ec02302a665d055d70ebcc5ebfbb3ccd407
Roy v.Oldenbeek <roy_v_oldenbeek@hotmail.com>
2016-06-15 10:49:12 +0200

    Added wristband activation/deactivation tests (and they work too xD)

--- EventsWebsite/EventsWebsite UnitTest/Database/ToegangTests.cs
+++ EventsWebsite/EventsWebsite UnitTest/Database/ToegangTests.cs
@@ -9,11 +9,21 @@ namespace EventsWebsite_UnitTest
     public class ToegangTests
     {
         [TestMethod]
-        public void TestCheckin()
+        public void TestAccess()
         {
             ToegangscontroleDatabase db = new ToegangscontroleDatabase();
             Assert.AreEqual(true, db.HasAccess(554491472),"Correct user has no access!");
             Assert.AreEqual(false,db.HasAccess(544923811),"Non existing user has access!");
+        }
+
+        public void TestActivation()
+        {
+            ToegangscontroleDatabase db = new ToegangscontroleDatabase();
+            int barcode = 554491472;
+            db.UpdateTag(barcode,"1");
+            Assert.AreEqual("1",db.ReadStringWithCondition("Polsbandje","Actief","barcode",barcode.ToString()),"Set 1 failed");
+            db.UpdateTag(barcode,"0");
+            Assert.AreEqual("0", db.ReadStringWithCondition("Polsbandje", "Actief", "barcode", barcode.ToString()), "Set 0 failed");
         }
     }
 }
