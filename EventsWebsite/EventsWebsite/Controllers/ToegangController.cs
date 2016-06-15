34a048121d7e774e7383fa4e27413d9fe0da88c6
Roy v.Oldenbeek <roy_v_oldenbeek@hotmail.com>
2016-06-15 13:16:10 +0200

    toegangscontrole jwz

--- EventsWebsite/EventsWebsite/Controllers/ToegangController.cs
+++ EventsWebsite/EventsWebsite/Controllers/ToegangController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EventsWebsite.Database;
 
 namespace EventsWebsite.Controllers
 {
     public class ToegangController : Controller
     {
+        private ToegangscontroleDatabase _db = new ToegangscontroleDatabase();
         
         public ActionResult Index()
         {
@@ -19,6 +21,23 @@ namespace EventsWebsite.Controllers
                 }
             }
             //return RedirectToAction("Index", "Home"); 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            int bar;
+            string barcode = form.Get(0);
+            if (int.TryParse(barcode, out bar))
+            {
+                bool granted = _db.HasAccess(bar);
+                if (granted)
+                {
+                    return RedirectToAction("Granted", "Toegang");
+                }
+                return RedirectToAction("Denied", "Toegang");
+            }
             return View();
         }
     }
