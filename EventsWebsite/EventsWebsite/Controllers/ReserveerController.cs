2a54dccbe61304874a80a415fe465c48750fb2e6
MvG013 <marcvangool@hotmail.com>
2016-06-18 15:33:10 +0200

    ReserveerDB

--- EventsWebsite/EventsWebsite/Controllers/ReserveerController.cs
+++ EventsWebsite/EventsWebsite/Controllers/ReserveerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EventsWebsite.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,7 @@ namespace EventsWebsite.Controllers
         // GET: Reserveer
         public ActionResult Index()
         {
+            ReserveerDB ResDB = new ReserveerDB();
             return View();
         }
 
