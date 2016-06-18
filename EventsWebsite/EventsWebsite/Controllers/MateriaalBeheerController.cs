a1d66c6c70f28ab4ea139e7b9ddb971790a9116d
sanderVE <sandervanelderen@gmail.com>
2016-06-18 15:38:00 +0200

    Views/controller materiaalverhuur

--- EventsWebsite/EventsWebsite/Controllers/MateriaalBeheerController.cs
+++ EventsWebsite/EventsWebsite/Controllers/MateriaalBeheerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EventsWebsite.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,17 +9,36 @@ namespace EventsWebsite.Controllers
 {
     public class MateriaalBeheerController : Controller
     {
+        private MateriaalverhuurDB database = new MateriaalverhuurDB();
+
         // GET: MateriaalBeheer
         public ActionResult Index()
         {
             if (Session.Count > 0)
             {
-                if ((int)Session["Niveau"] > 3)
-                {
-                    return View();
-                }
+                    return View(database.GetAllFreeMaterial());
             }
-            return View();
+            return View(database.GetAllFreeMaterial());
+        }
+
+        public ActionResult HireMaterial(int number)
+        {
+            database.ReserveMaterial(number, "06-06-2016");
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult HiredMaterials()
+        {
+            if (Session.Count > 0)
+            {
+                return View(database.GetAllHiredMaterial(1));
+            }
+            return View(database.GetAllHiredMaterial(1));
+        }
+
+        public ActionResult ReturnMaterial(int number)
+        {
+            return View("HiredMaterials", database.GetAllHiredMaterial(1));
         }
     }
 }
