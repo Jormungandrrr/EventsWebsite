78b47f30a660b290e910371aac0cfd09a2995e3c
MvG013 <marcvangool@hotmail.com>
2016-06-18 15:08:40 +0200

    Eventmodel aangepast en reserveer begin

---
+++ EventsWebsite/EventsWebsite/Controllers/ReserveerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventsWebsite.Controllers
+{
+    public class ReserveerController : Controller
+    {
+        // GET: Reserveer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Reserveer
+        public ActionResult Event()
+        {
+            return View();
+        }
+    }
+}
