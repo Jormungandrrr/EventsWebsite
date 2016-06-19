067677a855e954f687c2a95cf8dd9a6fa98d7345
coenvc <coenvc@gmail.com>
2016-06-19 16:44:53 +0200

    nud aangepast

--- EventsWebsite/EventsWebsite/Scripts/nud.js
+++ EventsWebsite/EventsWebsite/Scripts/nud.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
+    $("#fg0").hide();
     $("#fg1").hide();
     $("#fg2").hide();
     $("#fg3").hide();
@@ -6,6 +7,13 @@
     $("#fg5").hide();
     $("#fg6").hide();
     $("#buttongetnum").click(function () {
+        $("#fg0").hide();
+        $("#fg1").hide();
+        $("#fg2").hide();
+        $("#fg3").hide();
+        $("#fg4").hide();
+        $("#fg5").hide();
+        $("#fg6").hide();
         var amount = $("#numinput").val();
         for (i = 0; i < amount; i++) {
             $("#fg" + i).show();
